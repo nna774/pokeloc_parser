a4ddc529fa874124f6dde47b27863e0eb51aadde
Kugayama Nana <nonamea774@gmail.com>
2020-09-29 15:30:19 +0900

    init

---
+++ lib/pokeloc_parser.rb
@@ -0,0 +1,6 @@
+require "pokeloc_parser/version"
+
+module PokelocParser
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ spec/pokeloc_parser_spec.rb
@@ -0,0 +1,9 @@
+RSpec.describe PokelocParser do
+  it "has a version number" do
+    expect(PokelocParser::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(false).to eq(true)
+  end
+end
